7f47e80642d4043438af09f48829e6e38afd2292
Etienne-Meunier <etiennemeunier@live.fr>
2019-09-07 16:03:43 -0400

    package

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
-from setuptools import setup
+from distutils.core import setup
 
-setup(name='dracaufeu',
-      version='0.1',
-      description='Pytorch extension package for personal use>',
-      url='https://github.com/Etienne-Meunier',
-      author='Etienne Meunier',
-      author_email='etiennemeunier@live.fr',
-      license='MIT',
-      packages=['dracaufeu'],
-      zip_safe=False)
+setup(
+  name = 'dracaufeu',
+  packages = ['dracaufeu'],
+  version = '0.1',
+  license='MIT',
+  description = 'Pytorch extension package for personal use',
+  author = 'Etienne Meunier',
+  author_email = 'etiennemeunier@live.fr',
+  url = 'https://github.com/Etienne-Meunier/dracaufeu',
+  download_url = 'https://github.com/Etienne-Meunier/dracaufeu/archive/v_01.tar.gz',
+  keywords = ['Pytorch', 'DeepLearning'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
