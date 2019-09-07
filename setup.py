3b065b162ee0e055df8a68307328c902a37bd63c
Etienne-Meunier <etiennemeunier@live.fr>
2019-09-07 15:10:05 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dracaufeu',
+      version='0.1',
+      description='Pytorch extension package for personal use>',
+      url='https://github.com/Etienne-Meunier',
+      author='Etienne Meunier',
+      author_email='etiennemeunier@live.fr',
+      license='MIT',
+      packages=['dracaufeu'],
+      zip_safe=False)
